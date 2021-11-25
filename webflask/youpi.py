6f5516eea92d1082f8f650b8bc40b3072f7a0a3c
Dofyx <94303138+Dofyx@users.noreply.github.com>
2021-11-25 22:05:45 +0100

    Update youpi.py

--- webflask/youpi.py
+++ webflask/youpi.py
@@ -1,1 +1,14 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello world'
+
+@app.route('/play')
+def play():
+    subprocess.call(['./youpiPlay.sh'], shell=True) 
+  
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
