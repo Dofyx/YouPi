b8d8c0428649e7430fb59f6be1551db477bcc0d2
Dofyx <94303138+Dofyx@users.noreply.github.com>
2021-11-26 22:00:55 +0100

    Update youpi.py

--- webflask/youpi.py
+++ webflask/youpi.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
+
 @app.route('/play')
 def play():
     subprocess.call(['./youpiPlay.sh'], shell=True)
