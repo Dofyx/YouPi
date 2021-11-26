6c95f76494602e7d585257475584a24c129f0e41
Dofyx <94303138+Dofyx@users.noreply.github.com>
2021-11-26 18:15:31 +0100

    Update youpi.py

--- webflask/youpi.py
+++ webflask/youpi.py
@@ -9,11 +9,13 @@ def index():
 
 @app.route('/play')
 def play():
-    subprocess.call(['./youpiPlay.sh'], shell=True) 
+    subprocess.call(['./youpiPlay.sh'], shell=True)
+    return render_template('index.html')
   
 @app.route('/stop')
 def stop():
     subprocess.call(['./youpiStop.sh'], shell=True)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
