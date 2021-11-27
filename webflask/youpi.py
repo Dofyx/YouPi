1f8146b4a61fccda838931fd21fa6ee9cfed5968
Dofyx <94303138+Dofyx@users.noreply.github.com>
2021-11-27 12:46:12 +0100

    Update youpi.py

--- webflask/youpi.py
+++ webflask/youpi.py
@@ -28,4 +28,4 @@ def stop():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=8080)
