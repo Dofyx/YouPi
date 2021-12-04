ad757fa535ee055eb7827821175ee3df32025627
Dofyx <94303138+Dofyx@users.noreply.github.com>
2021-12-05 00:07:34 +0100

    Update youpi.sh

--- Scripts/youpi.sh
+++ Scripts/youpi.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-vlc --extraintf --telnet --telnet-password @youpi@ --telnet-host 127.0.0.1 --telnet-port 5824
+vlc --extraintf --telnet --telnet-password you314 --telnet-host 127.0.0.1 --telnet-port 5824
 python3 /home/pi/YouPi/webflask/youpi.py
