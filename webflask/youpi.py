780780a1ea9fe5e7970f5b0259ad31b7f47fb83c
Dofyx <94303138+Dofyx@users.noreply.github.com>
2021-12-14 00:01:48 +0100

    Update youpi.py

--- webflask/youpi.py
+++ webflask/youpi.py
@@ -1,61 +1,110 @@
 #!/usr/bin/python3
 
 # Flask
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import subprocess
 app = Flask(__name__)
 
 # Telnet
-import telnetlib
+import telnetlib, time
 host = '127.0.0.1'
 port = 5824
 passwd = 'you314'
-def tnconnect():
-    tn=telnetlib.Telnet(host, port)
-    tn.read_until(b"Password: ")
-    tn.write(passwd.encode('ascii') + b'\n')
 
 # API
 @app.route('/')
 def index():
-    return render_template('index.html')
+    tn=telnetlib.Telnet(host, port)
+    tn.read_until(b"Password: ")
+    tn.write(passwd.encode('ascii') + b'\n')
+    tn.write(b'playlist 1\n')
+    time.sleep(1)
+    output = tn.read_very_eager()
+    tn.close()
+    playlist = output.decode() + "\n"
+    return render_template('index.html', html_playlist=playlist)
 
 @app.route('/', methods=['POST'])
 def add():
     link = request.form['youtube_link']
-    tnconnect()
-    tn.write(b'add ' + str(link).encode('ascii') + b'\n')
+    tn=telnetlib.Telnet(host, port)
+    tn.read_until(b"Password: ")
+    tn.write(passwd.encode('ascii') + b'\n')
+    tn.write(b'enqueue ' + str(link).encode('ascii') + b'\n')
     tn.close()
+    return redirect("/")
 
-@app.route('/play', methods=['GET'])
+@app.route('/play', methods=['POST'])
 def play_api():
-    tnconnect()
+    tn=telnetlib.Telnet(host, port)
+    tn.read_until(b"Password: ")
+    tn.write(passwd.encode('ascii') + b'\n')
     tn.write(b'play\n')
     tn.close()
+    return redirect("/")
 
-@app.route('/stop', methods=['GET'])
+@app.route('/stop', methods=['POST'])
 def stop_api():
-    tnconnect()
+    tn=telnetlib.Telnet(host, port)
+    tn.read_until(b"Password: ")
+    tn.write(passwd.encode('ascii') + b'\n')
     tn.write(b'stop\n')
     tn.close()
+    return redirect("/")
 
-@app.route('/pause', methods=['GET'])
+@app.route('/pause', methods=['POST'])
 def pause_api():
-    tnconnect()
+    tn=telnetlib.Telnet(host, port)
+    tn.read_until(b"Password: ")
+    tn.write(passwd.encode('ascii') + b'\n')
     tn.write(b'pause\n')
     tn.close()
-    
-@app.route('/volup', methods=['GET'])
+    return redirect("/")
+
+@app.route('/next', methods=['POST'])
+def next_api():
+    tn=telnetlib.Telnet(host, port)
+    tn.read_until(b"Password: ")
+    tn.write(passwd.encode('ascii') + b'\n')
+    tn.write(b'next\n')
+    tn.close()
+    return redirect("/")
+
+@app.route('/prev', methods=['POST'])
+def prev_api():
+    tn=telnetlib.Telnet(host, port)
+    tn.read_until(b"Password: ")
+    tn.write(passwd.encode('ascii') + b'\n')
+    tn.write(b'prev\n')
+    tn.close()
+    return redirect("/")
+
+@app.route('/clear', methods=['POST'])
+def clear_api():
+    tn=telnetlib.Telnet(host, port)
+    tn.read_until(b"Password: ")
+    tn.write(passwd.encode('ascii') + b'\n')
+    tn.write(b'clear\n')
+    tn.close()
+    return redirect("/")
+
+@app.route('/volup', methods=['POST'])
 def volup_api():
-    tnconnect()
+    tn=telnetlib.Telnet(host, port)
+    tn.read_until(b"Password: ")
+    tn.write(passwd.encode('ascii') + b'\n')
     tn.write(b'volup 1\n')
     tn.close()
-    
-@app.route('/voldown', methods=['GET'])
+    return redirect("/")
+
+@app.route('/voldown', methods=['POST'])
 def voldown_api():
-    tnconnect()
+    tn=telnetlib.Telnet(host, port)
+    tn.read_until(b"Password: ")
+    tn.write(passwd.encode('ascii') + b'\n')
     tn.write(b'voldown 1\n')
     tn.close()
+    return redirect("/")
 
 # run server
 if __name__ == '__main__':
