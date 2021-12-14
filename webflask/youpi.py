cbfa58e4d7e0277175239efa4502d8f18b5268ff
Dofyx <94303138+Dofyx@users.noreply.github.com>
2021-12-14 07:54:21 +0100

    Update youpi.py

--- webflask/youpi.py
+++ webflask/youpi.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 # Flask
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, Response
 import subprocess
 app = Flask(__name__)
 
 # vlc instance
 import pexpect
-vlc = pexpect.spawn("vlc --no-video") #vlc instance to play the videos 
+vlc = pexpect.spawn("vlc --novideo")
 vlc.expect('>')
 
 # API
@@ -19,47 +19,57 @@ def index():
 @app.route('/', methods=['POST'])
 def add():
     link = request.form['youtube_link']
-   
+    add_cmd = "enqueue " + link # sends this command to the vlc instance 
+    vlc.sendline(add_cmd)
+    vlc.expect('>')
     return redirect("/")
 
 @app.route('/play', methods=['POST'])
 def play_api():
-    
+    vlc.sendline("play")
+    vlc.expect('>')
     return redirect("/")
 
 @app.route('/stop', methods=['POST'])
 def stop_api():
-    
+    vlc.sendline("stop")
+    vlc.expect('>')
     return redirect("/")
 
 @app.route('/pause', methods=['POST'])
 def pause_api():
-    
+    vlc.sendline("pause")
+    vlc.expect('>')
     return redirect("/")
 
 @app.route('/next', methods=['POST'])
 def next_api():
-   
+    vlc.sendline("next")
+    vlc.expect('>')
     return redirect("/")
 
 @app.route('/prev', methods=['POST'])
 def prev_api():
-    
+    vlc.sendline("prev")
+    vlc.expect('>')
     return redirect("/")
 
 @app.route('/clear', methods=['POST'])
 def clear_api():
-  
+    vlc.sendline("clear")
+    vlc.expect('>')
     return redirect("/")
 
 @app.route('/volup', methods=['POST'])
 def volup_api():
-   
+    vlc.sendline("volup 1")
+    vlc.expect('>')
     return redirect("/")
 
 @app.route('/voldown', methods=['POST'])
 def voldown_api():
-    
+    vlc.sendline("voldown 1")
+    vlc.expect('>')
     return redirect("/")
 
 # run server
